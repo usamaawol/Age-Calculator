81cc843f647a2058a117743605e7e7263c3dd360
Usama Awol <usamaawol0@gmail.com>
2026-04-08 23:10:41 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let userInput = document.getElementById("date");
+let resultOutput = document.getElementById("result");
+let calculateButton = document.querySelector("button");
+
+userInput.max = new Date().toISOString().split("T")[0];
+calculateButton.addEventListener("click", calculateAge);
+let result = document.getElementById("result");
+
+function calculateAge(){
+    let birthDate = new Date(userInput.value);
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date();
+    
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+    y3 = y2 - y1;
+
+
+    if(m2 >= m1){
+       m3 = m2 - m1;
+
+    }
+    else{
+        y3--;
+        m3 = 12 + m2 - m1;
+
+    }
+    if(d2 >= d1){
+        d3 = d2 - d1;
+    }else{
+        m3--;
+        d3 = getDaysInMonth(y2, m2) + d2 - d1;
+    }
+    if(m3 < 0){
+        m3 += 12;
+        y3--;
+    }
+    result.innerHTML  = `You are ${y3} years, ${m3} months and ${d3} days old `;
+    console.log(y3, m3, d3);
+function getDaysInMonth(year, month){
+        return new Date(year, month, 0).getDate();
+    }
+}
